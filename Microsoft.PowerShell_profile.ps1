d4aff6bc01752fef565212951b850106b3b3da04
Matthew Barnes <mbarnes@fedoraproject.org>
2021-05-31 10:50:08 -0400

    profile: Attempt slow-running jobs in parallel

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -13,20 +13,34 @@ foreach ($name in Get-Alias) {
 New-Alias -force -name kubectl -value \"Program Files"\Kubectl\kubectl
 New-Alias -force -name kc -value kubectl
 
-try {
-    if ((Get-AzContext) -eq $null) {
-        Write-Host "Connecting to Azure..."
-        Connect-AzAccount 3>$null  # Suppress warning stream
-    }
-} catch [System.Management.Automation.CommandNotFoundException] {
-    Write-Host -ForegroundColor Yellow "Az.Accounts cmdlets not found. Consider installing Azure PowerShell package."
-}
+# Attempt slow-running jobs in parallel.
+$jobs = @()
 
-Write-Output "Logging into Geneva Actions..."
-Login-GenevaActions -Env Public -RefreshToken
-$claims = Get-Claims
-Write-Host -NoNewline "Roles: "
-Write-Host -ForegroundColor Green "$(($claims | Where-Object Value -match "^ARO*").Value -join ', ')"
+$jobs += (Start-Job -ScriptBlock {
+    try {
+        if ((Get-AzContext) -eq $null) {
+            Write-Host "Connecting to Azure..."
+            Connect-AzAccount 3>$null  # Suppress warning stream
+        }
+    } catch [System.Management.Automation.CommandNotFoundException] {
+        Write-Host -ForegroundColor Yellow "Az.Accounts cmdlets not found. Consider installing Azure PowerShell package."
+    }
+})
+
+$jobs += (Start-Job -ScriptBlock {
+    Write-Output "Logging into Geneva Actions..."
+    Login-GenevaActions -Env Public -RefreshToken
+    $claims = Get-Claims
+    Write-Host -NoNewline "Roles: "
+    Write-Host -ForegroundColor Green "$(($claims | Where-Object Value -match "^ARO*").Value -join ', ')"
+})
+
+foreach ($job in $jobs) {
+    while ($job.HasMoreData -or $job.State -eq "Running") {
+        Receive-Job -Job $jobs
+        Start-Sleep -Seconds 1
+    }
+}
 
 # This is where kubeconfig files land.
 $env:KUBECONFIGDIR="$env:USERPROFILE\Work Folders\Downloads"
